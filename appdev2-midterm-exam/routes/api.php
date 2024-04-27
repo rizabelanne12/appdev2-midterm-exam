9dcfe9b9b35f25b636b7db504ec3c51f6bba3f7a
rizabelanne12 <rizabelannepili@student.laverdad.edu.ph>
2024-04-27 15:21:32 +0800

    update api

--- appdev2-midterm-exam/routes/api.php
+++ appdev2-midterm-exam/routes/api.php
@@ -21,12 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::middleware('extract.token')->group(function(){
-    // Route::get('/uploads', [ProductController::class, 'index']); 
-    // Route::post('/uploads', [ProductController::class, 'store']);  
-    // Route::get('uploads/{upload}', [ProductController::class, 'show']);
-    // Route::put('uploads/{upload}', [ProductController::class, 'update']);
-    // Route::delete('uploads/{upload}', [ProductController::class, 'destroy']);
-    Route::post('upload', [ProductController::class , 'uploadImagePublic']);
+    Route::get('/uploads', [ProductController::class, 'index']); 
+    Route::post('/uploads', [ProductController::class, 'store']);  
+    Route::get('uploads/{upload}', [ProductController::class, 'show']);
+    Route::put('uploads/{upload}', [ProductController::class, 'update']);
+    Route::delete('uploads/{upload}', [ProductController::class, 'destroy']);
+    // Route::post('upload', [ProductController::class , 'uploadImagePublic']);
     
 });
 
